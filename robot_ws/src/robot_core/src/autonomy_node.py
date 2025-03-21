0d18465a51736145a0352aa6a90ba7abe0cae5ba
Entity014 <radlove.012@gmail.com>
2025-03-21 12:36:14 +0700

    vghbjk

--- robot_ws/src/robot_core/src/autonomy_node.py
+++ robot_ws/src/robot_core/src/autonomy_node.py
@@ -69,7 +69,7 @@ class AutonomyBehavior(Node):
                         [0.1, 0.08, -95.0, False],
                     ),
                     LineFollowing(f"line1", tree.node, "F"),
-                    RobotMove(f"move1", tree.node, "F", 0.75),
+                    RobotMove(f"move1", tree.node, "F", 0.76),
                     RobotRotate(f"rotate1", -90, tree.node),
                     RobotArm(
                         f"arm2",
@@ -149,8 +149,8 @@ class AutonomyBehavior(Node):
                     RobotRotate(f"rotate7", 90, tree.node),
                     LineFollowingCircle("circle1", tree.node),  # ! Tuning
                     RobotMove(f"move7", tree.node, "F", 1.9),
-                    # LineFollowingCircle("circle2", tree.node),  # ! Tuning
-                    # RobotMove(f"move8", tree.node, "F", 1.9),
+                    LineFollowingCircle("circle2", tree.node),  # ! Tuning
+                    RobotMove(f"move8", tree.node, "F", 1.9),
                 ]
             )
         return tree
