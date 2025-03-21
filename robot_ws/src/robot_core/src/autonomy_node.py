c5c90f9edddd26535452dc5477a4cfffceece948
Entity014 <radlove.012@gmail.com>
2025-03-21 13:17:48 +0700

    tyuiop

--- robot_ws/src/robot_core/src/autonomy_node.py
+++ robot_ws/src/robot_core/src/autonomy_node.py
@@ -69,7 +69,7 @@ class AutonomyBehavior(Node):
                         [0.1, 0.08, -95.0, False],
                     ),
                     LineFollowing(f"line1", tree.node, "F"),
-                    RobotMove(f"move1", tree.node, "F", 0.8),
+                    RobotMove(f"move1", tree.node, "F", 0.82),
                     RobotRotate(f"rotate1", -90, tree.node),
                     RobotArm(
                         f"arm2",
